e0fee1808b380850f844154d2c25053583825e21
Glitch (hello-express) <none>
2021-03-24 05:09:15 +0000

    🛎🐸 Checkpoint ./public/js/client.js:30110384/1324

--- public/js/client.js
+++ public/js/client.js
@@ -4,7 +4,7 @@ var Promise = TrelloPowerUp.Promise;
 
 var BLACK_ROCKET_ICON = 'https://cdn.glitch.com/1b42d7fe-bda8-4af8-a6c8-eff0cea9e08a%2Frocket-ship.png?1494946700421';
 
-TrelloPowerUp.initialize({
+// TrelloPowerUp.initialize({
   // Start adding handlers for your capabilities here!
 	// 'card-buttons': function(t, options) {
 	// return t.set("member", "shared", "hello", "world")
@@ -21,5 +21,42 @@ TrelloPowerUp.initialize({
 	// 	  }];
 	// })
 	// },
+// });
+
+window.TrelloPowerUp.initialize({
+  "card-badges": function (t, opts) {
+    let cardAttachments = opts.attachments; // Trello passes you the attachments on the card
+    return t
+      .card("name")
+      .get("name")
+      .then(function (cardName) {
+        console.log("We just loaded the card name for fun: " + cardName);
+        return [
+          {
+            // Dynamic badges can have their function rerun
+            // after a set number of seconds defined by refresh.
+            // Minimum of 10 seconds.
+            dynamic: function () {
+              // we could also return a Promise that resolves to
+              // this as well if we needed to do something async first
+              return {
+                text: "Dynamic " + (Math.random() * 100).toFixed(0).toString(),
+                icon: "./images/icon.svg",
+                color: "green",
+                refresh: 10, // in seconds
+              };
+            },
+          },
+          {
+            // It's best to use static badges unless you need your
+            // badges to refresh.
+            // You can mix and match between static and dynamic
+            text: "Static",
+            // icon: HYPERDEV_ICON, // for card front badges only
+            color: null,
+          },
+        ];
+      });
+  },
 });
 
