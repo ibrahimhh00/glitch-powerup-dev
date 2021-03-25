ad83ccabbfd2ec9f27c8c52d228faf2b34ae39a5
Glitch (hello-express) <none>
2021-03-25 02:56:49 +0000

    📌👔 Checkpoint ./public/js/client.js:30110384/135

--- public/js/client.js
+++ public/js/client.js
@@ -73,10 +73,13 @@ window.TrelloPowerUp.initialize({
   },
   
   'card-badges': function(t, options) {
+    return t.get('card', 'shared', 'estimate')
+    .then(function(estimate) {
     return [{
       icon: 'https://cdn.glitch.com/c69415fd-f70e-4e03-b43b-98b8960cd616%2Frocket-ship-grey.png?1496162964717',
-      text: '3'
+      text: estimate
     }];
+});
   },
 
 });
