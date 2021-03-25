4ea8fd05ac957c44a0eb4c37049af3971c8ae9e1
Glitch (hello-express) <none>
2021-03-25 03:07:57 +0000

    🏦🐆 Checkpoint ./public/js/client.js:30110384/633

--- public/js/client.js
+++ public/js/client.js
@@ -1,5 +1,9 @@
 /* global TrelloPowerUp */
 
+var GREY_ROCKET_ICON = 'https://cdn.glitch.com/c69415fd-f70e-4e03-b43b-98b8960cd616%2Frocket-ship-grey.png?1496162964717';
+var WHITE_ROCKET_ICON = 'https://cdn.glitch.com/c69415fd-f70e-4e03-b43b-98b8960cd616%2Fwhite-rocket-ship.png?1495811896182';
+
+
 var Promise = TrelloPowerUp.Promise;
 
 var BLACK_ROCKET_ICON = 'https://cdn.glitch.com/1b42d7fe-bda8-4af8-a6c8-eff0cea9e08a%2Frocket-ship.png?1494946700421';
@@ -76,8 +80,10 @@ window.TrelloPowerUp.initialize({
     return t.get('card', 'shared', 'estimate')
     .then(function(estimate) {
     return [{
-      icon: 'https://cdn.glitch.com/c69415fd-f70e-4e03-b43b-98b8960cd616%2Frocket-ship-grey.png?1496162964717',
-      text: estimate
+      // icon: 'https://cdn.glitch.com/c69415fd-f70e-4e03-b43b-98b8960cd616%2Frocket-ship-grey.png?1496162964717',
+      icon: estimate ? GREY_ROCKET_ICON : null,
+      text: estimate,
+      color: estimate ? "blue" : null,
     }];
 });
   },
