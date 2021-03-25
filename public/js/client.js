0bd613324742335f6450491e20cdb43651fcfc24
Glitch (hello-express) <none>
2021-03-25 17:06:58 +0000

    🐔📉 Checkpoint ./public/js/estimate.js:30110384/24 ./public/js/client.js:30110384/14 ./public/estimate.html:30110384/18

--- public/js/client.js
+++ public/js/client.js
@@ -16,7 +16,7 @@ window.TrelloPowerUp.initialize({
   'card-buttons': function(t, options){
     return [{
       icon: 'https://cdn.glitch.com/1b42d7fe-bda8-4af8-a6c8-eff0cea9e08a%2Frocket-ship.png?1494946700421',
-      text: 'Estimate Size',
+      text: 'Backend Estimate Size',
       callback: function(t){
         return t.popup({
           title: "Backend Estimation",
--- public/js/estimate.js
+++ public/js/estimate.js
@@ -1,9 +1,9 @@
 var t = TrelloPowerUp.iframe();
 
-window.estimate.addEventListener('submit', function(event){
+window.backend_estimate.addEventListener('submit', function(event){
   // Stop the browser trying to submit the form itself.
   event.preventDefault();
-  return t.set('card', 'shared', 'backend_estimate', window.estimateSize.value)
+  return t.set('card', 'shared', 'backend_estimate', window.backend_estimateSize.value)
   .then(function(){
     t.closePopup();
   });
@@ -13,7 +13,7 @@ t.render(function(){
   
     return t.get('card', 'shared', 'backend_estimate')
   .then(function(backend_estimate){
-    window.estimateSize.value = backend_estimate;
+    window.backend_estimateSize.value = backend_estimate;
   })
   .then(function(){
   t.sizeTo('#backend_estimate').done();
