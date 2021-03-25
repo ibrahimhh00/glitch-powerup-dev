0543e3877a7096f6726e861a55b4cec95494ead8
Glitch (hello-express) <none>
2021-03-25 17:42:44 +0000

    📈🙍 Checkpoint ./public/js/estimate.js:30110384/360 ./public/estimate.html:30110384/9

--- public/js/estimate.js
+++ public/js/estimate.js
@@ -1,31 +1,9 @@
-// var t = TrelloPowerUp.iframe();
-
-// window.backend_estimate.addEventListener('submit', function(event){
-//   // Stop the browser trying to submit the form itself.
-//   event.preventDefault();
-//   return t.set('card', 'shared', 'backend_estimate', window.backend_estimateSize.value)
-//   .then(function(){
-//     t.closePopup();
-//   });
-// });
-
-// t.render(function(){
-  
-//     return t.get('card', 'shared', 'backend_estimate')
-//   .then(function(backend_estimate){
-//     window.backend_estimateSize.value = backend_estimate;
-//   })
-//   .then(function(){
-//   t.sizeTo('#backend_estimate').done();
-//     });
-// });
-
 var t = TrelloPowerUp.iframe();
 
 window.backend_estimate.addEventListener('submit', function(event){
   // Stop the browser trying to submit the form itself.
   event.preventDefault();
-  return t.set('card', 'shared', window.resourceLabel, window.backend_estimateSize.value)
+  return t.set('card', 'shared', 'backend_estimate', window.backend_estimateSize.value)
   .then(function(){
     t.closePopup();
   });
@@ -35,9 +13,41 @@ t.render(function(){
   
     return t.get('card', 'shared', 'backend_estimate')
   .then(function(backend_estimate){
+      console.log(JSON.stringify(backend_estimate, null, 2));
     window.backend_estimateSize.value = backend_estimate;
   })
   .then(function(){
   t.sizeTo('#backend_estimate').done();
     });
 });
+
+
+
+
+
+// var t = TrelloPowerUp.iframe();
+
+// window.backend_estimate.addEventListener('submit', function(event){
+//   // Stop the browser trying to submit the form itself.
+//   event.preventDefault();
+//   return t.set('card', 'shared', window.resourceLabel, window.backend_estimateSize.value)
+//   .then(function(){
+//     t.closePopup();
+//   });
+// });
+
+// t.render(function(){
+  
+//   return t.get('board', 'shared')
+// .then(function (data) {
+//   console.log(JSON.stringify(data, null, 2));
+// });
+  
+//   //   return t.get('card', 'shared', 'backend_estimate')
+//   // .then(function(backend_estimate){
+//   //   window.backend_estimateSize.value = backend_estimate;
+//   // })
+//   // .then(function(){
+//   // t.sizeTo('#backend_estimate').done();
+//   //   });
+// });
