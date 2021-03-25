a6750642cca137b6943c3f7d1e4649141384d452
Glitch (hello-express) <none>
2021-03-25 17:30:52 +0000

    ⛽️📇 Checkpoint ./public/js/client.js:30110384/358 ./public/resource.html:30110384/1143 ./public/js/resource.js:30110384/571 ./public/estimate.html:30110384/64 ./public/js/estimate.js:30110384/789

--- public/js/estimate.js
+++ public/js/estimate.js
@@ -1,9 +1,31 @@
+// var t = TrelloPowerUp.iframe();
+
+// window.backend_estimate.addEventListener('submit', function(event){
+//   // Stop the browser trying to submit the form itself.
+//   event.preventDefault();
+//   return t.set('card', 'shared', 'backend_estimate', window.backend_estimateSize.value)
+//   .then(function(){
+//     t.closePopup();
+//   });
+// });
+
+// t.render(function(){
+  
+//     return t.get('card', 'shared', 'backend_estimate')
+//   .then(function(backend_estimate){
+//     window.backend_estimateSize.value = backend_estimate;
+//   })
+//   .then(function(){
+//   t.sizeTo('#backend_estimate').done();
+//     });
+// });
+
 var t = TrelloPowerUp.iframe();
 
 window.backend_estimate.addEventListener('submit', function(event){
   // Stop the browser trying to submit the form itself.
   event.preventDefault();
-  return t.set('card', 'shared', 'backend_estimate', window.backend_estimateSize.value)
+  return t.set('card', 'shared', window.resourceLabel, window.backend_estimateSize.value)
   .then(function(){
     t.closePopup();
   });
