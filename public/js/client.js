8934babaf80126b952a1a9d6bb5bc54c9051bab9
Glitch (hello-express) <none>
2021-03-27 22:01:57 +0000

    🎑🏎 Checkpoint ./public/js/client.js:30110384/13303

--- public/js/client.js
+++ public/js/client.js
@@ -10,114 +10,80 @@ var ROCKET_ICON = 'https://cdn.glitch.com/aef55bcb-cec2-438b-98c0-959249969810%2
 
 var Promise = TrelloPowerUp.Promise;
 
+// var onBtnClick = function (t, opts) {
+
+//   return t.popup({
+//     title: 'Snooze Card',
+//     items: function(t, options) {
+   
+//          // We want to retrieve all of the cards we currently have and all of the fields
+//       // on those cards that we will want to use for searching through.
+//         return t.cards('id','idList', 'name').then(function (cards) {
+//           const searchText = "apility";  // The text the user has input.
+//           const matchedCards = cards.filter(function(card){
+//           // We need to shrink our list of possible matches to those cards that contain what the
+//           // user has input. We'll use a naive approach here and just see if the string entered
+//           // is in any of the fields we care about.
+//           const textToSearch = card.id + card.idList + card.name;
+//           var tempT = textToSearch.toLowerCase().includes(searchText.toLowerCase());
+//           console.log("tempT is:", tempT)
+//           return textToSearch.toLowerCase().includes(searchText.toLowerCase());
+//         })
+//         // Once we have all of the cards that match our search criteria, we need to put them into
+//         // the array of objects that the t.popup method expects.
+//           let items = matchedCards.map(function(card){
+//           const cardUrl = `https://trello.com/c/${card.id}`
+//           return {
+//             text: card.name,
+//             url: cardUrl,
+//             callback: function(t){
+//               // When the user selects one of the cards we've returned in the search, we want
+//               // to attach that card via it's URL.
+//               return t.attach({ url: cardUrl, name: card.name })
+//               .then(function(){
+//                 // Once we've attached the card's URL to the current card, we can close
+//                 // our search popup.
+//                 return t.closePopup();
+//               });
+//             }
+//           }
+//         })                  
+//         return items;          //don't forget to return items
+             
+//                 });
+//     },
+//     search: {
+//       placeholder: 'Card name, description, or ID',
+//       empty: 'Huh, nothing there 🤔',
+//       searching: 'Searching your cards...'
+//     }
+//   });
+// };
+  
+
 var onBtnClick = function (t, opts) {
   // console.log('Someone clicked the button');
   return t.popup({
     title: 'Snooze Card',
     items: function(t, options) {
       
-    // t.lists("all").get("")  
-      
-      
-//           return t.lists("all").then(function (lists) {
-//       console.log(JSON.stringify(lists, null, 2));
-            
-//              return t.get("5f53e15a6bb8a9122694687f", "shared","backend_estimate").then(function (estimate) {
-//               console.log("estimate is:", estimate)
-//             });
-//     });
 
-         // We want to retrieve all of the cards we currently have and all of the fields
-      // on those cards that we will want to use for searching through.
-        return t.cards('id','idList', 'name').then(function (cards) {
-          const searchText = "apility";  // The text the user has input.
-          const matchedCards = cards.filter(function(card){
-          // We need to shrink our list of possible matches to those cards that contain what the
-          // user has input. We'll use a naive approach here and just see if the string entered
-          // is in any of the fields we care about.
-          const textToSearch = card.id + card.idList + card.name;
-          var tempT = textToSearch.toLowerCase().includes(searchText.toLowerCase());
-          console.log("tempT is:", tempT)
-          return textToSearch.toLowerCase().includes(searchText.toLowerCase());
-        })
-        // Once we have all of the cards that match our search criteria, we need to put them into
-        // the array of objects that the t.popup method expects.
-          let items = matchedCards.map(function(card){
-          const cardUrl = `https://trello.com/c/${card.id}`
-          return {
-            text: card.name,
-            url: cardUrl,
-            callback: function(t){
-              // When the user selects one of the cards we've returned in the search, we want
-              // to attach that card via it's URL.
-              return t.attach({ url: cardUrl, name: card.name })
-              .then(function(){
-                // Once we've attached the card's URL to the current card, we can close
-                // our search popup.
-                return t.closePopup();
-              });
-            }
-          }
-        })                  
-        return items;          //don't forget to return items
+                return t.cards('id','idList', 'name').then(function (cards) {
+                
+          
                 
                 // console.log(JSON.stringify(cards, null, 2))
-                // console.log("ids are:", cards);
+                console.log("card type is:", typeof cards);
+                console.log("Cards keyes:", cards.keys())
             
                 });
-    },
-    search: {
-      placeholder: 'Card name, description, or ID',
-      empty: 'Huh, nothing there 🤔',
-      searching: 'Searching your cards...'
+      
     }
   });
 };
-  
 
-// var onBtnClick = function (t, opts) {
-//   // console.log('Someone clicked the button');
-//   return t.popup({
-//     title: 'Snooze Card',
-//     items: function(t, options) {
-      
-//     // t.lists("all").get("")  
-      
-      
-// //           return t.lists("all").then(function (lists) {
-// //       console.log(JSON.stringify(lists, null, 2));
-            
-// //              return t.get("5f53e15a6bb8a9122694687f", "shared","backend_estimate").then(function (estimate) {
-// //               console.log("estimate is:", estimate)
-// //             });
-// //     });
 
-//                 return t.cards('id','idList', 'name').then(function (cards) {
-                
-                  
-                
-//                 console.log(JSON.stringify(cards, null, 2))
-//                 console.log("ids are:", cards);
-            
-//                 });
-       
-      
-//        // return t.getAll().then(function (estimate) {
-//        //    console.log("estimate is:", estimate)
-//        //  });
-      
-      
-//     //    return t.cards("id","idList","name","badges","customFieldItems","coordinates","pos").then(function (cards) {
-//     //   console.log(JSON.stringify(cards, null, 2));
-//     // });
-      
-//     //  return t.board("id", "name","customFields").then(function (board) {
-//     //   console.log(JSON.stringify(board, null, 2));
-//     // });
-      
-//     }
-//   });
-// };
+
 
 window.TrelloPowerUp.initialize({
     
