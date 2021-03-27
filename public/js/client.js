2112f203ae18a644723eb1a48a8283a4fae24d9d
Glitch (hello-express) <none>
2021-03-27 02:46:40 +0000

    🐔🎊 Checkpoint ./public/js/client.js:30110384/1883

--- public/js/client.js
+++ public/js/client.js
@@ -11,8 +11,35 @@ var ROCKET_ICON = 'https://cdn.glitch.com/aef55bcb-cec2-438b-98c0-959249969810%2
 var Promise = TrelloPowerUp.Promise;
 
 
+var onBtnClick = function (t, opts) {
+  console.log('Someone clicked the button');
+};
 
 window.TrelloPowerUp.initialize({
+    
+   'board-buttons': function (t, opts) {
+    return [{
+      // we can either provide a button that has a callback function
+      icon: {
+        dark: WHITE_ROCKET_ICON,
+        light: BLACK_ROCKET_ICON
+      },
+      text: 'Callback',
+      callback: onBtnClick,
+      condition: 'edit'
+    }, {
+      // or we can also have a button that is just a simple url
+      // clicking it will open a new tab at the provided url
+      icon: {
+        dark: WHITE_ROCKET_ICON,
+        light: BLACK_ROCKET_ICON
+      },
+      text: 'URL',
+      condition: 'always',
+      url: 'https://trello.com/inspiration',
+      target: 'Inspiring Boards' // optional target for above url
+    }];
+  },
  
   'card-buttons': function(t, options){
     return [{
