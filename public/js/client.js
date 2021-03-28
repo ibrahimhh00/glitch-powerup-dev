581360f4ed7162c7b725e8db5cb7de7bc82990be
Glitch (hello-express) <none>
2021-03-28 03:02:42 +0000

    🏔✌️ Checkpoint ./public/js/client.js:30110384/123

--- public/js/client.js
+++ public/js/client.js
@@ -71,13 +71,11 @@ var onBtnClick = function (t, opts) {
                 // t.cards return a set of values, 
                 //values are in object of nested array, 
                 //values of outer array assign to tempArray 
-                //then values of inner array assign to an array of card IDs
-                
+                //then values of inner array assigned to an array of card IDs as keys
                 return t.cards('id','idList', 'name').then(function (cards) {
      
                 // console.log(JSON.stringify(cards, null, 2))
-                  
-                // Nested Array object, Assign object values of the outer array to an array            
+                            
                 var tempArray = Object.values(cards)
                 console.log("Cards values :", tempArray)
                 var cardEstimateArr =  new Map();
