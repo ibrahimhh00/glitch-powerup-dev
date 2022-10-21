34c15523f2402750bc5081605690d1cc5c5cbf16
Glitch (hello-express) <none>
2022-10-21 09:44:20 +0000

    🎡🧀 Checkpoint ./public/js/client.js:49891700/14

--- public/js/client.js
+++ public/js/client.js
@@ -168,8 +168,8 @@ window.TrelloPowerUp.initialize({
       
       console.log(estimates);
       
-      var backend_estimate =   12 // estimates["card"]["shared"]["backend_estimate"];
-      var frontend_estimate =   15 // estimates["card"]["shared"]["frontend_estimate"];
+      var backend_estimate =   estimates["card"]["shared"]["backend_estimate"];
+      var frontend_estimate =   estimates["card"]["shared"]["frontend_estimate"];
 
       return [
         {
