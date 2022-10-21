3757c48c4c7914f24f74c59e7be20628a8f5a8d5
Glitch (hello-express) <none>
2022-10-21 11:18:34 +0000

    🍗🎒 Checkpoint ./public/js/estimate.js:49891700/5644

--- public/js/estimate.js
+++ public/js/estimate.js
@@ -1,70 +1,63 @@
 var t = TrelloPowerUp.iframe();
 
-
-window.backend_estimate.addEventListener('submit', function(event){
+window.backend_estimate.addEventListener("submit", function (event) {
   let object_estimateSize = {
-  backend_estimate: window.backend_estimateSize.value,
-  frontend_estimate: window.frontend_estimateSize.value,
-}
+    backend_estimate: window.backend_estimateSize.value,
+    frontend_estimate: window.frontend_estimateSize.value,
+  };
 
   // Stop the browser trying to submit the form itself.
   event.preventDefault();
-  return t.set('card', 'shared', object_estimateSize)
-  
-  .then(function(){
+  return t
+    .set("card", "shared", object_estimateSize)
 
-    t.closePopup();
-  });
-});
-
-
-
-// t.render is called when there is an update, here it i used 
-// If you open the popup via the card button, select a size, 
-// hit the button, and then re-open the popup, you'll notice that your previous selection isn't initially selected. 
-// When the iframe loads, we'll want to check to see if there is a value that has already been set, 
-// and if so pre-select it for our list. Adding this leaves our 
-t.render(function(){
-  
-  
-//     return t.get('card', 'shared', 'backend_estimate')
-//   .then(function(backend_estimate){
-//       console.log("testtt");
-//       console.log(JSON.stringify(backend_estimate, null, 2));
-//     window.backend_estimateSize.value = backend_estimate;
-//   })
-  
-//   .then(function(){
-//   t.sizeTo('#backend_estimate').done();
-//     });
-// });
-
-
-  return t.getAll()
-    // return t.get('card', 'shared', 'backend_estimate')
-  .then(function(estimates){
-      console.log(Object.keys(estimates["card"]["shared"]));
-      console.log(JSON.stringify(estimates, null, 2));
-    
-    // var object_estimate = Object.keys(estimates["card"]["shared"])
-    // console.log("object_estimate: ", object_estimate[1])
-    // console.log("object_estimat type: ", typeof object_estimate)
-    
-    console.log("sss",((estimates["card"]["shared"]["backend_estimate"])))
-    window.backend_estimateSize.value = estimates["card"]["shared"]["backend_estimate"];
-    window.frontend_estimateSize.value = estimates["card"]["shared"]["frontend_estimate"];
-  })
-  
-  .then(function(){
-  t.sizeTo('#backend_estimate').done();
+    .then(function () {
+      t.closePopup();
     });
-
-
 });
 
+// t.render is called when there is an update, here it i used
+// If you open the popup via the card button, select a size,
+// hit the button, and then re-open the popup, you'll notice that your previous selection isn't initially selected.
+// When the iframe loads, we'll want to check to see if there is a value that has already been set,
+// and if so pre-select it for our list. Adding this leaves our
+t.render(function () {
+  //     return t.get('card', 'shared', 'backend_estimate')
+  //   .then(function(backend_estimate){
+  //       console.log("testtt");
+  //       console.log(JSON.stringify(backend_estimate, null, 2));
+  //     window.backend_estimateSize.value = backend_estimate;
+  //   })
 
+  //   .then(function(){
+  //   t.sizeTo('#backend_estimate').done();
+  //     });
+  // });
 
+  return (
+    t
+      .getAll()
+      // return t.get('card', 'shared', 'backend_estimate')
+      .then(function (estimates) {
+        console.log(Object.keys(estimates["card"]["shared"]));
+        console.log(JSON.stringify(estimates, null, 2));
 
+        // var object_estimate = Object.keys(estimates["card"]["shared"])
+        // console.log("object_estimate: ", object_estimate[1])
+        // console.log("object_estimat type: ", typeof object_estimate)
+
+        console.log("sss", estimates["card"]["shared"]["backend_estimate"]);
+        window.backend_estimateSize.value =
+          estimates["card"]["shared"]["backend_estimate"];
+        window.frontend_estimateSize.value =
+          estimates["card"]["shared"]["frontend_estimate"];
+      })
+
+      .then(function () {
+        t.sizeTo("#backend_estimate").done();
+      })
+  );
+});
 
 // var t = TrelloPowerUp.iframe();
 
@@ -78,12 +71,12 @@ t.render(function(){
 // });
 
 // t.render(function(){
-  
+
 //   return t.get('board', 'shared')
 // .then(function (data) {
 //   console.log(JSON.stringify(data, null, 2));
 // });
-  
+
 //   //   return t.get('card', 'shared', 'backend_estimate')
 //   // .then(function(backend_estimate){
 //   //   window.backend_estimateSize.value = backend_estimate;
