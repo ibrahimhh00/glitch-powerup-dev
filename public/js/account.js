98dcedcd934847aa30a176700a149d4cb230934f
Glitch (hello-express) <none>
2023-07-21 10:34:47 +0000

    ⛰🦃 Checkpoint ./public/js/account.js:59410896/2506 ./public/js/category.js:59410896/14

---
+++ public/js/account.js
@@ -0,0 +1,68 @@
+const API_URL = "https://jsonplaceholder.typicode.com/users/";
+
+var t = window.TrelloPowerUp.iframe();
+
+$(document).ready(function () {
+  fetchMembers();
+});
+
+function fetchMembers() {
+  $.ajax({
+    url: API_URL,
+    type: "GET",
+    success: function (data) {
+      console.log(data);
+      populateMembers(data);
+    },
+    error: function (error) {
+      console.error("Error fetching members", error);
+    },
+  });
+}
+
+function populateMembers(members) {
+  t.get("card", "shared", "memberSizing").then(function (memberSizing) {
+    // memberSizing now contains the sizing data for members
+    let memberIdsWithSizing = memberSizing.map((ms) => ms.memberId);
+    console.log("memberIdsWithSizing", memberIdsWithSizing)
+
+    const membersList = $("#members");
+    members.forEach(function (member) {
+      // Exclude members that have sizing data
+      console.log(member.id)
+      if (!memberIdsWithSizing.includes(String(member.id))) {
+        const option = `<option value="${member.id}">${member.name}</option>`;
+        membersList.append(option);
+      }
+    });
+  });
+}
+
+$("#account").submit(function (event) {
+  event.preventDefault();
+
+  const selectedMemberId = $("#accounts").val();
+  const selectedMemberName = $("#accounts option:selected").text();
+  if(!sizing || !selectedMemberName) {
+    return
+  }
+  console.log(selectedMemberId, sizing);
+  t.get("card", "shared", "memberSizing", [])
+    .then(function (memberSizing) {
+      // memberSizing will be an empty array if it's not set yet.
+      // Now add the new member to it
+      memberSizing.push({
+        memberId: selectedMemberId,
+        memberName: selectedMemberName,
+        sizing: sizing,
+      });
+
+      // Now save it back
+      return t.set("card", "shared", "memberSizing", memberSizing);
+    })
+    .then(() => {
+      console.log("New member stored.");
+      return t.closePopup();
+    });
+  // t.closePopup();
+});
