4e109f807e8b9c48f16c8ed78fd9fdad54d58277
Glitch (hello-express) <none>
2023-10-09 18:53:39 +0000

    📼💓 Checkpoint ./public/js/sizing.js:59410896/15516

--- public/js/sizing.js
+++ public/js/sizing.js
@@ -1,4 +1,24 @@
 const API_URL = "http://localhost:9000/api/v1";
+var ENDPOINT_URL = "http://localhost:9000/api/v1";
+var GREY_ROCKET_ICON =
+  "https://cdn.glitch.com/c69415fd-f70e-4e03-b43b-98b8960cd616%2Frocket-ship-grey.png?1496162964717";
+var WHITE_ROCKET_ICON =
+  "https://cdn.glitch.com/c69415fd-f70e-4e03-b43b-98b8960cd616%2Fwhite-rocket-ship.png?1495811896182";
+var BLACK_ROCKET_ICON =
+  "https://cdn.glitch.com/1b42d7fe-bda8-4af8-a6c8-eff0cea9e08a%2Frocket-ship.png?1494946700421";
+
+var BACKEND_ICON =
+  "https://cdn.glitch.com/aef55bcb-cec2-438b-98c0-959249969810%2Fworksheet-number-monochrome-b-classroom.jpg?v=1616731943091";
+// var FRONTEND_ICONn
+var ROCKET_ICON =
+  "https://cdn.glitch.com/aef55bcb-cec2-438b-98c0-959249969810%2Fc69415fd-f70e-4e03-b43b-98b8960cd616_white-rocket-ship.png?v=1616729876518";
+
+var DISRUPTEM_ICON1 =
+  "https://cdn.glitch.com/bcb67d52-05a1-4b6e-a315-f5bae36b69eb%2FIcon-Color%403x.png?v=1625811265010";
+var DISRUPTEM_ICON2 =
+  "https://cdn.glitch.com/bcb67d52-05a1-4b6e-a315-f5bae36b69eb%2F1.png?v=1625811412559";
+var DISRUPTEM_ICON3 =
+  "https://cdn.glitch.com/bcb67d52-05a1-4b6e-a315-f5bae36b69eb%2Fdisruptem-Icon_White.png?v=1625811831046";
 
 var t = window.TrelloPowerUp.iframe();
 
@@ -82,7 +102,87 @@ $("#estimate").submit(async function (event) {
 
     if (response.ok) {
       console.log("Success:", await response.json());
+      window.TrelloPowerUp.initialize({
+ mcolor: "red",
+                    refresh: 10,
+                    callback: function (t) {
+                      return t.popup({
+                        title: "Adjust Member Sizing",
+                        items: [
+                          {
+                            text: "Member: " + member.memberId.name,
+                          },
+                          {
+                            text: "Current Sizing: " + member.sizing,
+                            callback: function (t) {
+                              return t.popup({
+                                url: "input",
+                                title: "Adjust Sizing",
+                                url: `../adjust-size.html?cardId=${cardId.id}&idList=${idList.id}&idBoard=${idBoard.id}&memberId=${member.memberId.id}&memberName=${member.memberId.name}&currentSizing=${member.sizing}`,
+                              });
+                            },
+                          },
+                          {
+                            text: "Delete Member",
+                            callback: function (t) {
+                              const data = {
+                                memberId: member.memberId._id,
+                                cardId: cardId.id
+                              }
+                              fetch(`${ENDPOINT_URL}/cards/delete-member`, {
+                                method: "POST", // Specifying the HTTP method
+                                headers: {
+                                  "Content-Type": "application/json", // Setting the content type of the request
+                                },
+                                body: JSON.stringify(data), // Converting the data to a JSON string
+                              })
+                                .then((response) => response.json()) // Parsing the JSON response from the server
+                                .then((data) => {
+                                  console.log("Success:", data); // Handling the successful response
+                                })
+                                .catch((error) => {
+                                  console.error("Error:", error); // Handling errors
+                                });
+                              console.log(
+                                "Deleting member with ID: ",
+                                member.memberId._id,
+                                cardId.id
+                              );
+                              // Implement your logic here to delete the member from the card
+                            },
+                          },
+                        ],
+                      });
+                    },
+                  };
+                },
+              };
+            });
+            const categoriesBadges = data.data.members
+              .filter(
+                (member, index, self) =>
+                  index ===
+                  self.findIndex(
+                    (m) =>
+                      m.memberId.category.id === member.memberId.category.id
+                  )
+              )
+              .map((member) => {
+                return {
+                  text: member.memberId.category.name,
+                  color: member.memberId.category.color,
+                };
+              });
+            return [...membersBadges, ...categoriesBadges];
+          });
+      }
+    );
+  },
+});
+      window.location.reload();
       t.closePopup();
+      window.location.reload();
+
     } else {
       console.error("Server responded with status", response.status);
     }
