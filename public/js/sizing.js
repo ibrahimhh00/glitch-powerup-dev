515842955d1feb42e162ce7e9730c04c0e92c215
Glitch (hello-express) <none>
2023-10-18 13:32:05 +0000

    🎖👴 Checkpoint ./public/js/sizing.js:59410896/282

--- public/js/sizing.js
+++ public/js/sizing.js
@@ -1,5 +1,5 @@
-const API_URL = "http://localhost:9000/api/v1";
-var ENDPOINT_URL = "http://localhost:9000/api/v1";
+var API_URL = "http://localhost:9000/api/v1";
+// var API_URL = "https://uxperts-backend-staging.disruptem.com/api/v1"
 var GREY_ROCKET_ICON =
   "https://cdn.glitch.com/c69415fd-f70e-4e03-b43b-98b8960cd616%2Frocket-ship-grey.png?1496162964717";
 var WHITE_ROCKET_ICON =
@@ -170,7 +170,7 @@ $("#estimate").submit(async function (event) {
                             memberId: member._id,
                             cardId: data.cardId,
                           };
-                          fetch(`${ENDPOINT_URL}/cards/delete-member`, {
+                          fetch(`${API_URL}/cards/delete-member`, {
                             method: "POST", // Specifying the HTTP method
                             headers: {
                               "Content-Type": "application/json", // Setting the content type of the request
